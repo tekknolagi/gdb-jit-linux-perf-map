f16160bf77862819e1fdc24c2d6805737e08776f
Max Bernstein <max@bernsteinbear.com>
2025-12-30 17:08:00 -0500

    Add perf/samply

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <sys/mman.h> /* for mmap and friends */
 #include <stdio.h>    /* for fprintf */
 #include <stdlib.h>   /* for EXIT_SUCCESS */
+#include <stdint.h>   /* for uint64_t, uint32_t */
+#include <sys/types.h> /* for pid_t */
+#include <unistd.h>    /* for getpid */
 
 const unsigned char program[] = {
     // mov eax, 42 (0x2a)
@@ -16,7 +19,18 @@ const int kProgramSize = sizeof program;
 
 typedef int (*JitFunction)();
 
+void register_with_perf(const char *code_name, void *code_addr, size_t code_size) {
+  pid_t pid = getpid();
+  char filename[256];
+  snprintf(filename, sizeof(filename), "/tmp/perf-%d.map", pid);
+  FILE *f = fopen(filename, "a+");
+  assert(f && "fopen failed");
+  fprintf(f, "%lx %zx %s\n", (uintptr_t)code_addr, code_size, code_name);
+  fclose(f);
+}
+
 int main() {
+  fprintf(stderr, "my pid is %d\n", getpid());
   void *memory = mmap(/*addr=*/NULL, /*length=*/kProgramSize,
                       /*prot=*/PROT_READ | PROT_WRITE,
                       /*flags=*/MAP_ANONYMOUS | MAP_PRIVATE,
@@ -24,6 +38,7 @@ int main() {
   assert(memory != MAP_FAILED && "mmap failed");
   memcpy(memory, program, kProgramSize);
   int result = mprotect(memory, kProgramSize, PROT_EXEC);
+  register_with_perf("my_jit_function", memory, kProgramSize);
   assert(result == 0 && "mprotect failed");
   JitFunction function = *(JitFunction*)&memory;
   int return_code = function();
